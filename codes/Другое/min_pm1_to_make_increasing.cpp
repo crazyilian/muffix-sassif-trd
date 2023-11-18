663fdcfdd82c8e6709c745627d32001eeaa9aa32
sevlll <82900814+sevlll@users.noreply.github.com>
2023-11-18 23:53:01 +0300

    Add files via upload

---
+++ codes/Другое/min_pm1_to_make_increasing.cpp
@@ -0,0 +1,17 @@
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int ans = 0;
+    multiset<int> now;
+    for (int i = 0; i < n; i++) {
+        now.insert(a[i]);
+        ans += (*now.rbegin() - a[i]);
+        now.erase(now.find(*now.rbegin()));
+        now.insert(a[i]);
+    }
+    cout << ans << '\n';
+}
