b6c5c9cb50a52c9bf27e490e025aee40d45fa593
sevlll <82900814+sevlll@users.noreply.github.com>
2023-11-23 23:30:24 +0300

    SOS !

--- codes/ДП/SOS-dp.cpp
+++ codes/ДП/SOS-dp.cpp
@@ -1,2 +1,25 @@
-// TODO
+// dp initial fill, a[] is given array, mb extra zeros
+for (int i = 0; i < (1 << N); i++) {
+  dp[i] = a[i];
+}
 
+// Classic SOS-dp, goal: dp[mask] = \sum a[submasks of mask]
+for (int i = 0; i < N; i++) {
+  for (int mask = 0; mask < (1 << N); mask++) {
+    if ((mask >> i) & 1) {
+      dp[mask] += dp[mask ^ (1 << i)];
+    }
+  }
+}
+
+// Overmasks SOS-dp, goal: dp[mask] = \sum a[overmasks of mask]
+for (int i = 0; i < N; i++) {
+  for (int mask = (1 << N) - 1; mask >= 0; mask--) {
+    if (((mask >> i) & 1) == 0) {
+      dp[mask] += dp[mask ^ (1 << i)];
+    }
+  }
+}
+
+// to inverse SOS-dp (restore original array by SOS-dp array):
+// use same code, but -= instead of += in dp transitions
