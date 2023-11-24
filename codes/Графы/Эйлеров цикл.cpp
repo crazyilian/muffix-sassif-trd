3513b69a5d2126cf27629f17a401fd3352a103f0
sevlll <82900814+sevlll@users.noreply.github.com>
2023-11-24 12:32:21 +0300

    Eulerian Circuit (non recursion version with set)

--- codes/Графы/Эйлеров цикл.cpp
+++ codes/Графы/Эйлеров цикл.cpp
@@ -1,2 +1,21 @@
-// TODO
-
+// unconnected graph, deleting edges, set<int> g[N];
+for (int v = 0; v < n; v++) {
+  if (!g[v].empty()) {
+    vector<int> ccl;
+    vector<int> s = {v};
+    while (!s.empty()) {
+      int u = s.back();
+      if (g[u].empty()) {
+        ccl.pb(u);
+        s.pop_back();
+      } else {
+        int u2 = *g[u].begin();
+        g[u].erase(u2);
+        g[u2].erase(u);
+        s.pb(u2);
+      }
+    }
+    // ccl[0] = ccl.back()
+    // i.e for graph with edges (1, 2), (1, 3), (2, 3) -> ccl = [1, 2, 3, 1]
+  }
+}
