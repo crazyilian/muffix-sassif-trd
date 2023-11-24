101a098ed5a21f6471068120c0353e1d44e2f81c
Ilian Andrianov <ilian.and.04@gmail.com>
2023-11-24 21:38:21 +0300

    фенвик на отрезке

---
+++ codes/Структуры данных/Фенвик (+ на отрезке).cpp
@@ -0,0 +1,18 @@
+// a[l..r) += x
+void update(int l, int r, int x) {
+  T1.add(l, x);
+  T1.add(r, -x);
+  T2.add(l, -x * l);
+  T2.add(r, x * r);
+}
+
+// sum a[0..pos)
+int rsq(int pos) {
+  return T1.sum(pos) * pos + T2.sum(pos);
+}
+
+// sum a[l..r)
+int sum(int l, int r) {
+  return rsq(r) - rsq(l);
+}
+
