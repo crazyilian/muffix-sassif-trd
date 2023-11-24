3b222b635011a545b0a900d43bb052fc24ddd530
tem-shett <115083378+tem-shett@users.noreply.github.com>
2023-11-24 20:14:27 +0300

    тук-тук "это КТО?"

--- codes/Математика/КТО.cpp
+++ codes/Математика/КТО.cpp
@@ -1,2 +1,21 @@
-// TODO
-
+// x = a_i % p_i
+  vector<vector<int>> r(k, vector<int>(k));
+  for (int i = 0; i < k; ++i) 
+    for (int j = 0; j < k; ++j) 
+      if (i != j) 
+        r[i][j] = binpow(p[i] % p[j], p[j] - 2, p[j]);
+  vector<int> x(k);
+  for (int i = 0; i < k; ++i) {
+    x[i] = a[i];
+    for (int j = 0; j < i; ++j) {
+      x[i] = r[j][i] * (x[i] - x[j]);
+      x[i] = x[i] % p[i];
+      if (x[i] < 0) x[i] += p[i];
+    }
+  }
+  int ans = 0;
+  for (int i = 0; i < k; ++i) {
+    int val = x[i];
+    for (int j = 0; j < i; ++j) val *= p[j];
+    ans += val;
+  }
