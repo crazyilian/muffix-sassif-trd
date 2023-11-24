00ffa0952e0a1f82c43e4a28e4ee14f6efe74efe
Ilian Andrianov <ilian.and.04@gmail.com>
2023-11-25 01:37:02 +0300

    Форматирование Ро-Полларда

--- codes/Математика/Ро-Поллард.cpp
+++ codes/Математика/Ро-Поллард.cpp
@@ -1,15 +1,17 @@
-long long mult(long long a, long long b, long long mod) {
+typedef long long ll;
+
+ll mult(ll a, ll b, ll mod) {
   return (__int128)a * b % mod;
 }
 
-long long f(long long x, long long c, long long mod) {
+ll f(ll x, ll c, ll mod) {
   return (mult(x, x, mod) + c) % mod;
 }
 
-long long rho(long long n, long long x0=2, long long c=1) {
-  long long x = x0;
-  long long y = x0;
-  long long g = 1;
+ll rho(ll n, ll x0=2, ll c=1) {
+  ll x = x0;
+  ll y = x0;
+  ll g = 1;
   while (g == 1) {
     x = f(x, c, n);
     y = f(y, c, n);
@@ -34,7 +36,7 @@ void factor(int n, vector<int> &pr) {
     pr.push_back(n);
     return;
   }
-  int d = rho(n, abs((int) rnd()) % (n - 2) + 2, abs((int) rnd()) % 3 + 1);
+  int d = rho(n, rnd() % (n - 2) + 2, rnd() % 3 + 1);
   factor(n / d, pr);
   factor(d, pr);
 }
