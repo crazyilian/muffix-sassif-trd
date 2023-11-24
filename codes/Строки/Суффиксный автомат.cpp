ec26d3ae40147e503c45ffc3f40b6f97dc7d78f1
Ilian Andrianov <ilian.and.04@gmail.com>
2023-11-24 23:23:46 +0300

    Update Суффиксный автомат.cpp

--- codes/Строки/Суффиксный автомат.cpp
+++ codes/Строки/Суффиксный автомат.cpp
@@ -1,53 +1,56 @@
-struct Node {
-    int go[26];
-    int suf;
-    int len;
-};
-Node verts[MAXN];
-int cnt_v = 1;
-int max_v = 0;
+const int SIGMA = 26;
+int ans = 0;
 
-void add(char c) {
-    c -= 'a';
-    int nv = cnt_v++;
-    verts[max_v].go[c] = nv;
-    verts[nv].len = verts[max_v].len + 1;
-    int v = max_v;
-    while (v != 0) {
-        if (verts[verts[v].suf].go[c] == 0) {
-            v = verts[v].suf;
-            verts[v].go[c] = nv;
-            verts[nv].len = max(verts[nv].len, verts[v].len + 1);
-            continue;
-        }
-        int vv = verts[v].suf, uu = verts[vv].go[c];
-        if (verts[vv].len + 1 == verts[uu].len) {
-            verts[nv].suf = uu;
-            break;
-        }
-        int v2 = cnt_v++;
-        for (int c2 = 0; c2 < 26; c2++) {
-            verts[v2].go[c2] = verts[uu].go[c2];
-        }
-        int to = verts[vv].go[c];
-        do {
-            if (verts[vv].go[c] == to) {
-                verts[vv].go[c] = v2;
-                verts[v2].len = max(verts[v2].len, verts[vv].len + 1);
-            } else {
-                break;
-            }
-            vv = verts[vv].suf;
-        } while (vv != 0);
-        if (verts[vv].go[c] == to) {
-            verts[vv].go[c] = v2;
-            verts[v2].len = max(verts[v2].len, verts[vv].len + 1);
-        }
-        verts[v2].suf = verts[uu].suf;
-        verts[uu].suf = verts[nv].suf = v2;
-        break;
+struct Node {
+  int go[SIGMA];
+  int s, p;
+  int len;
+
+  Node() {
+    fill(go, go + SIGMA, -1);
+    s = -1, p = -1;
+    len = 0;
+  }
+};
+
+int add(int A, int ch, vector<Node> &sa) {
+  int B = sa.size();
+  sa.emplace_back();
+  sa[B].p = A;
+  sa[B].s = 0;
+  sa[B].len = sa[A].len + 1;
+  for (; A != -1; A = sa[A].s) {
+    if (sa[A].go[ch] == -1) {
+      sa[A].go[ch] = B;
+      continue;
     }
-    max_v = nv;
+    int C = sa[A].go[ch];
+    if (sa[C].p == A) {
+      sa[B].s = C;
+      break;
+    }
+    int D = sa.size();
+    sa.emplace_back();
+    sa[D].s = sa[C].s;
+    sa[D].p = A;
+    sa[D].len = sa[A].len + 1;
+    sa[C].s = D;
+    sa[B].s = D;
+    copy(sa[C].go, sa[C].go + SIGMA, sa[D].go);
+    for (; A != -1 && sa[A].go[ch] == C; A = sa[A].s)
+      sa[A].go[ch] = D;
+    break;
+  }
+  ans += sa[B].len - sa[sa[B].s].len;
+  return B;
 }
 
-
+signed main() {
+  string s;
+  cin >> s;
+  vector<Node> sa(1);
+  int A = 0;
+  for (char c : s)
+    A = add(A, c - 'a', sa);
+  cout << ans;
+}
