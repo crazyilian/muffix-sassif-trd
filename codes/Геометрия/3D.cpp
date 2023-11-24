d5e7536f2841720b55e9f14e9c254c6bf3dcfb65
tem-shett <115083378+tem-shett@users.noreply.github.com>
2023-11-25 01:39:09 +0300

    Update 3D.cpp

--- codes/Геометрия/3D.cpp
+++ codes/Геометрия/3D.cpp
@@ -1,5 +1,25 @@
 struct Pt {
-  double x, y, z;
+  double x;
+  double y;
+  double z;
+
+  Pt(double x_, double y_, double z_) : x(x_), y(y_), z(z_) {}
+
+  Pt operator-(const Pt& other) const {
+    return {x - other.x, y - other.y, z - other.z};
+  }
+
+  Pt operator+(const Pt& other) const {
+    return {x + other.x, y + other.y, z + other.z};
+  }
+
+  Pt operator/(const double& a) const {
+    return {x / a, y / a, z / a};
+  }
+
+  Pt operator*(const double& a) const {
+    return {x * a, y * a, z * a};
+  }
 
   Pt cross(const Pt& p2) const {
     double nx = y * p2.z - z * p2.y;
@@ -7,15 +27,38 @@ struct Pt {
     double nz = x * p2.y - y * p2.x;
     return {nx, ny, nz};
   }
+
+  bool operator==(const Pt& pt) const {
+    return abs(x - pt.x) < eps && abs(y - pt.y) < eps && abs(z - pt.z) < eps;
+  }
+
+  double dist() {
+    return sqrtl(x * x + y * y + z * z);
+  }
 };
 
 struct Plane {
   double a, b, c, d;
-  
+
+  Plane(double a_, double b_, double c_, double d_) : a(a_), b(b_), c(c_), d(d_) {}
+
   double get_val(Pt p) {
     return a * p.x + b * p.y + c * p.z + d;
   }
+
+  bool on_plane(Pt p) {
+    return abs(get_val(p)) / hypot(a, b, c) < eps;
+  }
+
+  Pt proj(Pt p) {
+    double t = (a * p.x + b * p.y + c * p.z + d) / (a * a + b * b + c * c);
+    return p - Pt(a, b, c) * t;
+  }
 };
+
+bool on_line(Pt p1, Pt p2, Pt p3) {
+  return (p2 - p1).cross(p3 - p1) == Pt(0, 0, 0);
+}
 
 Plane get_plane(Pt p1, Pt p2, Pt p3) {
   Pt norm = (p2 - p1).cross(p3 - p1);
