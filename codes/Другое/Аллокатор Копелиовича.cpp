cd66d6be5a94d41d5358d8067b1de1dff9605856
Ilian Andrianov <ilian.and.04@gmail.com>
2023-11-24 23:28:53 +0300

    Update Аллокатор Копелиовича.cpp

--- codes/Другое/Аллокатор Копелиовича.cpp
+++ codes/Другое/Аллокатор Копелиовича.cpp
@@ -1,2 +1,15 @@
-// TODO
+// Код вставить до инклюдов
 
+#include <cassert>
+
+const int MAX_MEM = 1e8; // ~100mb
+int mpos = 0;
+char mem[MAX_MEM];
+
+inline void *operator new(std::size_t n) {
+  assert((mpos += n) <= MAX_MEM);
+  return (void *)(mem + mpos - n);
+}
+
+inline void operator delete(void *) noexcept {} // must have!
+inline void operator delete(void *, std::size_t) noexcept {} // fix!!
