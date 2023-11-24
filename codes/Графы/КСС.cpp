1f93fe60c8e092a5ad5fcae8f0bcd3e512fc5afb
tem-shett <115083378+tem-shett@users.noreply.github.com>
2023-11-24 17:44:30 +0300

    ксс для 2сат

--- codes/Графы/КСС.cpp
+++ codes/Графы/КСС.cpp
@@ -1,33 +1,29 @@
-void dfs1(int v) {
-    vis[v] = 1;
-    for (auto u : g[v]) {
-        if (!vis[u]) {
-            dfs1(u);
-        }
+void dfs1(int v, vector<int> &topsort) {
+  used[v] = 1;
+  for (auto u : g[v]) {
+    if (!used[u]) {
+      dfs1(u, topsort);
     }
-    topsort.push_back(v);
+  }
+  topsort.push_back(v);
 }
 
-void dfs2(int v) {
-    vis[v] = 1;
-    for (auto u : rg[v]) {
-        if (!vis[u]) {
-            dfs2(u);
-        }
+void dfs2(int v, int col) {
+  comp[v] = col;
+  for (auto u : rg[v]) {
+    if (!comp[u]) {
+      dfs2(u, col);
     }
-    comp.push_back(v);
+  }
 }
 
 signed main() {
-    for (int i = 1; i <= n; i++)
-        if (!vis[i])
-            dfs1(i);
-    reverse(topsort.begin(), topsort.end());
-    for (int j = 1; j <= n; j++) {
-        int vert = topsort[j - 1];
-        if (!vis[vert])
-            dfs2(vert);
-    }
+  vector<int> topsort;
+  for (int v = 1; v <= n; ++v)
+    if (!used[v])
+      dfs1(v, topsort);
+  reverse(all(topsort));
+  for (int j = 1; j <= n; ++j)
+    if (!comp[topsort[j - 1]])
+      dfs2(topsort[j - 1], j);
 }
-
-
