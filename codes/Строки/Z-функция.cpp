ae98f32f7b835050132d4089cbf764c5d010ffd9
tem-shett <115083378+tem-shett@users.noreply.github.com>
2023-11-25 22:05:49 +0300

    рефактор z функции

--- codes/Строки/Z-функция.cpp
+++ codes/Строки/Z-функция.cpp
@@ -1,18 +1,19 @@
-int main() {
-    vector<int> z(n, 0);
-    z[0] = n;
-    int l = 0, r = 0;
-    for (int i = 1; i < n; i++) {
-        if (i < r) {
-            z[i] = min(z[i - l], r - i);
-        }
-        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
-            z[i]++;
-        }
-        if (i + z[i] > r) {
-            l = i;
-            r = i + z[i];
-        }
+vector<int> z_func(string s) {
+  int n = s.size();
+  vector<int> z(n, 0);
+  z[0] = n;
+  int l = 0, r = 0;
+  for (int i = 1; i < n; i++) {
+    if (i < r) {
+      z[i] = min(z[i - l], r - i);
     }
+    while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+      z[i]++;
+    }
+    if (i + z[i] > r) {
+      l = i;
+      r = i + z[i];
+    }
+  }
+  return z;
 }
-
