21e1299cece9546c8de91a73cfee2383f99348da
tem-shett <115083378+tem-shett@users.noreply.github.com>
2023-11-25 23:22:58 +0300

    рефактор центроиды

--- codes/Деревья/Centroid.cpp
+++ codes/Деревья/Centroid.cpp
@@ -1,51 +1,51 @@
 void sizes(int v, int p) {
-    sz[v] = 1;
-    for (auto u : g[v]) {
-        if (u != p && !used[u]) {
-            sizes(u, v);
-            sz[v] += sz[u];
-        }
+  sz[v] = 1;
+  for (auto u : g[v]) {
+    if (u != p && !used[u]) {
+      sizes(u, v);
+      sz[v] += sz[u];
     }
+  }
 }
+
 int centroid(int v, int p, int n) {
-    for (int u : g[v]) {
-        if (sz[u] > n / 2 && u != p && !used[u]) {
-            return centroid(u, v, n);
-        }
+  for (int u : g[v]) {
+    if (sz[u] > n / 2 && u != p && !used[u]) {
+      return centroid(u, v, n);
     }
-    return v;
+  }
+  return v;
 }
 
 void dfs(int v, int p) {
-    ........
-    for (auto u : g[v]) {
-        if (u != p && !used[u]) {
-            dfs(u, v);
-        }
+  // code
+  for (auto u : g[v]) {
+    if (u != p && !used[u]) {
+      dfs(u, v);
     }
+  }
 }
 
 void solve(int v) {
-    sizes(v, -1);
-    .........
-    for (auto u : g[v]) {
-        if (!used[u]) {
-            ...........
-            dfs(u, v);   
-            ..................     
-        }
+  sizes(v, -1);
+  // code (?)
+  for (auto u : g[v]) {
+    if (!used[u]) {
+      // code (?)
+      dfs(u, v);
+      // сохраняем результаты dfs
     }
-    .....................
-    used[v] = 1;
-    for (int u : g[v]) {
-        if (!used[u]) {
-            solve(centroid(u, v, sz[u]));
-        }
+  }
+  // code (сливаем результаты dfs)
+  used[v] = 1;
+  for (int u : g[v]) {
+    if (!used[u]) {
+      solve(centroid(u, v, sz[u]));
     }
-}
-int main() {
-    sizes(0, -1);
-    solve(centroid(0, -1, n));
+  }
 }
 
-
+signed main() {
+  sizes(0, -1);
+  solve(centroid(0, -1, n));
+}
