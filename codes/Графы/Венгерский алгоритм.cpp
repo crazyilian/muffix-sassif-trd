3179109d1971262663d2cfd5bf67a6e69503703a
tem-shett <115083378+tem-shett@users.noreply.github.com>
2023-11-25 21:50:14 +0300

    Update Венгерский алгоритм.cpp

--- codes/Графы/Венгерский алгоритм.cpp
+++ codes/Графы/Венгерский алгоритм.cpp
@@ -1,43 +1,44 @@
-vector<int> venger(vector<vector<int>> arr) {
-    int n = (int) arr.size() - 1;
-    vector<int> u(n + 1), v(n + 1), p(n + 1), way(n + 1);
-    for (int i = 1; i <= n; i++) {
-        p[0] = i;
-        int ind = 0;
-        vector<int> minv(n + 1, INF), used(n + 1);
-        do {
-            used[ind] = 1;
-            int ind2 = p[ind], dlt = INF, ind3 = 0;
-            for (int j = 1; j <= n; j++)
-                if (!used[j]) {
-                    int cur = arr[ind2][j] - u[ind2] - v[j];
-                    if (cur < minv[j]) {
-                        minv[j] = cur;
-                        way[j] = ind;
-                    }
-                    if (minv[j] < dlt) {
-                        dlt = minv[j], ind3 = j;
-                    }
-                }
-            for (int j = 0; j <= n; j++)
-                if (used[j]) {
-                    u[p[j]] += dlt;
-                    v[j] -= dlt;
-                } else {
-                    minv[j] -= dlt;
-                }
-            ind = ind3;
-        } while (p[ind] != 0);
-        do {
-            int ind3 = way[ind];
-            p[ind] = p[ind3];
-            ind = ind3;
-        } while (ind);
-    }
-    vector<int> ans(n + 1);
-    for (int j = 1; j <= n; j++) {
-        ans[p[j]] = j;
-    }
-    return ans;
+pair<int, vector<int>> venger(vector<vector<int>> a) {
+// ищет минимальное по стоимости
+// работает только при n <= m
+// a - массив весов $(n+1) \times (m+1)$
+// a[0][..] = a[..][0] = 0
+// возвращает ans[i] = j если взяли ребро a[i][j]
+  int n = (int) a.size() - 1;
+  int m = (int) a[0].size() - 1;
+  vector<int> u(n + 1), v(m + 1), p(m + 1), way(m + 1);
+  for (int i = 1; i <= n; ++i) {
+    p[0] = i;
+    int j0 = 0;
+    vector<int> minv(m + 1, INF);
+    vector<char> used(m + 1, false);
+    do {
+      used[j0] = true;
+      int i0 = p[j0], delta = INF, j1;
+      for (int j = 1; j <= m; ++j)
+        if (!used[j]) {
+          int cur = a[i0][j] - u[i0] - v[j];
+          if (cur < minv[j])
+            minv[j] = cur, way[j] = j0;
+          if (minv[j] < delta)
+            delta = minv[j], j1 = j;
+        }
+      for (int j = 0; j <= m; ++j)
+        if (used[j])
+          u[p[j]] += delta, v[j] -= delta;
+        else
+          minv[j] -= delta;
+      j0 = j1;
+    } while (p[j0] != 0);
+    do {
+      int j1 = way[j0];
+      p[j0] = p[j1];
+      j0 = j1;
+    } while (j0);
+  }
+  int cost = -v[0];
+  vector<int> ans(n + 1);
+  for (int j = 1; j <= m; ++j)
+    ans[p[j]] = j;
+  return {cost, ans};
 }
-
