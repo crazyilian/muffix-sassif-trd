37240275c9e0eafe1b17fcc2baa66156efb223c4
tem-shett <115083378+tem-shett@users.noreply.github.com>
2023-11-25 22:54:58 +0300

    рефактор парсоча

--- codes/Графы/Паросочетания.cpp
+++ codes/Графы/Паросочетания.cpp
@@ -1,24 +1,24 @@
-bool dfs(int v, int c) {
-    if (used[v] == c) return false;
-    used[v] = c;
-    for (auto u : g[v]) {
-        if (res[u] == -1) {
-            res[u] = v;
-            return true;
-        }
+int dfs(int v, int c) {
+  if (used[v] == c) return 0;
+  used[v] = c;
+  for (auto u : g[v]) {
+    if (res[u] == -1) {
+      res[u] = v;
+      return 1;
     }
-    for (auto u : g[v]) {
-        if (dfs(res[u], c)) {
-            res[u] = v;
-            return true;
-        }
+  }
+  for (auto u : g[v]) {
+    if (dfs(res[u], c)) {
+      res[u] = v;
+      return 1;
     }
-    return false;
+  }
+  return 0;
 }
 
 signed main() {
-    for (int i = 0; i < s; ++i) {
-        ans += dfs(i, i + 1);
-    }
+  fill(res, res + n, -1);
+  for (int i = 0; i < n; ++i) {
+    ans += dfs(i, i + 1);
+  }
 }
-
