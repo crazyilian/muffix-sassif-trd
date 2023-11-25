36cc4053ad64207d60024a0bcf8fbc8c51070978
tem-shett <115083378+tem-shett@users.noreply.github.com>
2023-11-25 22:49:29 +0300

    рефактор корася

--- codes/Строки/Ахо-Корасик.cpp
+++ codes/Строки/Ахо-Корасик.cpp
@@ -1,40 +1,45 @@
+int go[MAXN][ALPH];
+vector<int> term[MAXN];
+int par[MAXN], suf[MAXN];
+char par_c[MAXN];
+vector<int> g[MAXN];
+
 int cntv = 1;
 
 void add(string &s) {
-    static int cnt_s = 1;
-    int v = 0;
-    for (char el : s) {
-        if (go[v][el - 'a'] == 0) {
-            go[v][el - 'a'] = cntv;
-            par[cntv] = v;
-            par_c[cntv] = el;
-            cntv++;
-        }
-        v = go[v][el - 'a'];
+  static int cnt_s = 1;
+  int v = 0;
+  for (char el: s) {
+    if (go[v][el - 'a'] == 0) {
+      go[v][el - 'a'] = cntv;
+      par[cntv] = v;
+      par_c[cntv] = el;
+      cntv++;
     }
-    term[v].push_back(cnt_s++);
+    v = go[v][el - 'a'];
+  }
+  term[v].push_back(cnt_s++);
 }
 
 void bfs() {
-    deque<int> q = {0};
-    while (!q.empty()) {
-        int v = q.front();
-        q.pop_front();
-        if (v > 0) {
-            if (par[v] == 0) {
-                suf[v] = 0;
-            } else {
-                suf[v] = go[suf[par[v]]][par_c[v] - 'a'];
-            }
-            g[suf[v]].push_back(v);
-        }
-        for (int c = 0; c < 26; c++) {
-            if (go[v][c] == 0) {
-                go[v][c] = go[suf[v]][c];
-            } else {
-                q.push_back(go[v][c]);
-            }
-        }
+  deque<int> q = {0};
+  while (!q.empty()) {
+    int v = q.front();
+    q.pop_front();
+    if (v > 0) {
+      if (par[v] == 0) {
+        suf[v] = 0;
+      } else {
+        suf[v] = go[suf[par[v]]][par_c[v] - 'a'];
+      }
+      g[suf[v]].push_back(v);
     }
+    for (int c = 0; c < 26; c++) {
+      if (go[v][c] == 0) {
+        go[v][c] = go[suf[v]][c];
+      } else {
+        q.push_back(go[v][c]);
+      }
+    }
+  }
 }
-
