2883c221bce474d970d52cfd6f38044c1663cd6b
tem-shett <115083378+tem-shett@users.noreply.github.com>
2023-11-25 23:06:47 +0300

    рефактор эдмондс-карп

--- codes/Графы/Эдмондс-Карп.cpp
+++ codes/Графы/Эдмондс-Карп.cpp
@@ -1,61 +1,62 @@
 struct edge {
-    int v, f, c, ind;
+  int v, f, c, ind;
 };
 
 vector<edge> g[MAXN];
 
 bool bfs(int start, int final, int W) {
-    vector<int> d(MAXN, INF);
-    vector<pair<int, int>> pred(MAXN);
-    d[start] = 0;
-    deque<int> q = {start};
-    while (!q.empty()) {
-        int v = q.front();
-        q.pop_front();
-        for (int i = 0; i < (int) g[v].size(); i++) {
-            auto e = g[v][i];
-            if (e.f + W <= e.c && d[e.v] > d[v] + 1) {
-                d[e.v] = d[v] + 1;
-                pred[e.v] = {v, i};
-                q.push_back(e.v);
-            }
-        }
+  vector<int> d(MAXN, INF);
+  vector<pair<int, int>> pred(MAXN);
+  d[start] = 0;
+  deque<int> q = {start};
+  while (!q.empty()) {
+    int v = q.front();
+    q.pop_front();
+    for (int i = 0; i < (int) g[v].size(); i++) {
+      auto e = g[v][i];
+      if (e.f + W <= e.c && d[e.v] > d[v] + 1) {
+        d[e.v] = d[v] + 1;
+        pred[e.v] = {v, i};
+        q.push_back(e.v);
+      }
     }
-    if (d[final] == INF) {
-        return false;
-    }
-    int v = final;
-    int x = INF;
-    while (v != start) {
-        int ind = pred[v].second;
-        v = pred[v].first;
-        x = min(x, g[v][ind].c - g[v][ind].f);
-    }
-    v = final;
-    while (v != start) {
-        int ind = pred[v].second;
-        v = pred[v].first;
-        g[v][ind].f += x;
-        g[g[v][ind].v][g[v][ind].ind].f -= x;
-    }
-    return true;
+  }
+  if (d[final] == INF) {
+    return false;
+  }
+  int v = final;
+  int x = INF;
+  while (v != start) {
+    int ind = pred[v].second;
+    v = pred[v].first;
+    x = min(x, g[v][ind].c - g[v][ind].f);
+  }
+  v = final;
+  while (v != start) {
+    int ind = pred[v].second;
+    v = pred[v].first;
+    g[v][ind].f += x;
+    g[g[v][ind].v][g[v][ind].ind].f -= x;
+  }
+  return true;
 }
 
 signed main() {
-    for (int i = 0; i < m; i++) {
-        int u, v, c;
-        cin >> u >> v >> c;
-        g[u].push_back({v, 0, c, (int) g[v].size()});
-        g[v].push_back({u, 0, 0, (int) g[u].size() - 1});
-    }
-    int W = (1 << 30);
-    do {
-        while (bfs(1, n, W));
-        W /= 2;
-    } while (W >= 1);
-    int res = 0;
-    for (auto e : g[1]) {
-        res += e.f;
-    }
+  int n, m;
+  for (int i = 0; i < m; i++) {
+    int u, v, c;
+    cin >> u >> v >> c;
+    g[u].push_back({v, 0, c, (int) g[v].size()});
+    g[v].push_back({u, 0, 0, (int) g[u].size() - 1});
+  }
+  int start = 1, final = n;
+  int W = (1 << 30);
+  do {
+    while (bfs(start, final, W));
+    W /= 2;
+  } while (W >= 1);
+  int res = 0;
+  for (auto e : g[start]) {
+    res += e.f;
+  }
 }
-
