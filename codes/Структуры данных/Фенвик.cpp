c23f68b7f8fe1634c922856bce76676d1bc4c5dc
Ilian Andrianov <ilian.and.04@gmail.com>
2024-03-05 03:29:33 +0300

    remove square dijkstra & rewrite fenwick

--- codes/Структуры данных/Фенвик.cpp
+++ codes/Структуры данных/Фенвик.cpp
@@ -1,44 +1,46 @@
 // Нумерация с 0
 
-struct FenwickTree {
+struct Fenwick {
   int n;
-  vector<vector<vector<int>>> ft;
-
-  FenwickTree(int n) : n(n) {
-    ft.resize(n + 1, vector<vector<int>>(n + 1, vector<int>(n + 1)));
+  vector<int> f;
+ 
+  Fenwick(int n) : n(n) {
+    f.resize(n + 1);
   }
-
-  // a[x][y][z] += d
-  void upd(int x, int y, int z, int d) {
-    x++, y++, z++;
-    for (int x1 = x; x1 <= n; x1 += x1 & -x1) {
-      for (int y1 = y; y1 <= n; y1 += y1 & -y1) {
-        for (int z1 = z; z1 <= n; z1 += z1 & -z1) {
-          ft[x1][y1][z1] += d;
-        }
-      }
-    }
+ 
+  // a[i] += x
+  void add(int i, int x) {
+    for (++i; i <= n; i += i & -i)
+      f[i] += x;
   }
-
-  // sum a[0..x)[0..y)[0..z)
-  int rsq(int x, int y, int z) {
+ 
+  // sum a[0..i)
+  int get(int i) {
     int ans = 0;
-    for (int x1 = x; x1 > 0; x1 -= x1 & -x1) {
-      for (int y1 = y; y1 > 0; y1 -= y1 & -y1) {
-        for (int z1 = z; z1 > 0; z1 -= z1 & -z1) {
-          ans += ft[x1][y1][z1];
-        }
-      }
-    }
+    for (; i > 0; i -= i & -i)
+      ans += f[i];
     return ans;
   }
-
-  // sum a[x1..x2)[y1..y2)[z1..x2)
-  int sum_3d(int x1, int x2, int y1, int y2, int z1, int z2) {
-    int ans = rsq(x2, y2, z2);
-    ans -= rsq(x1, y2, z2) + rsq(x2, y1, z2) + rsq(x2, y2, z1);
-    ans += rsq(x1, y1, z2) + rsq(x1, y2, z1) + rsq(x2, y1, z1);
-    ans -= rsq(x1, y1, z1);
-    return ans;
+ 
+  // a[..] > 0; find max k: sum a[0..k) <= x
+  int max_not_more(int x) {
+    int cur = 0;
+    for (int i = 20; i >= 0; --i) {
+      int len = 1 << i;
+      if (cur + len <= n && f[cur + len] <= x) {
+        cur += len;
+        x -= f[cur];
+      }
+    }
+    return cur;
   }
 };
+
+// sum a[x1..x2)[y1..y2)[z1..x2)
+int sum_3d(int x1, int x2, int y1, int y2, int z1, int z2) {
+  int ans = get(x2, y2, z2);
+  ans -= get(x1, y2, z2) + get(x2, y1, z2) + get(x2, y2, z1);
+  ans += get(x1, y1, z2) + get(x1, y2, z1) + get(x2, y1, z1);
+  ans -= get(x1, y1, z1);
+  return ans;
+}
