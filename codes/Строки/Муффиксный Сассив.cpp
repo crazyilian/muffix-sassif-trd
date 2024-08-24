8915487363b7dd16db3d421724bad1a2817a492a
tem-shett <115083378+tem-shett@users.noreply.github.com>
2024-08-24 14:34:58 +0300

    норм МУФСАС

--- codes/Строки/Муффиксный Сассив.cpp
+++ codes/Строки/Муффиксный Сассив.cpp
@@ -1,66 +1,55 @@
-vector<int> build_suff_arr(string s) {
-  int MAX = max((int) s.size() + 10, (int) 256);
-  s.push_back('#');
+vector<int> build_suff_arr(string &s) {
+  // Remove, if you want to sort cyclic shifts
+  s += (char) (1);
   int n = s.size();
-  vector<int> suf(n), c(n);
-  vector<int> cnt(MAX);
+  vector<int> a(n);
+  iota(all(a), 0);
+  stable_sort(all(a), [&](int i, int j) {
+      return s[i] < s[j];
+  });
+  vector<int> c(n);
+  int cc = 0;
   for (int i = 0; i < n; i++) {
-    cnt[s[i] - '#']++;
-  }
-  vector<int> pos(MAX);
-  for (int i = 1; i < MAX; i++) {
-    pos[i] = pos[i - 1] + cnt[i - 1];
-  }
-  for (int i = 0; i < n; i++) {
-    suf[pos[s[i] - '#']++] = i;
-  }
-  int cls = -1;
-  for (int i = 0; i < n; i++) {
-    if (i == 0 || s[suf[i]] != s[suf[i - 1]]) {
-      cls++;
+    if (i == 0 || s[a[i]] != s[a[i - 1]]) {
+      c[a[i]] = cc++;
+    } else {
+      c[a[i]] = c[a[i - 1]];
     }
-    c[suf[i]] = cls;
   }
   for (int L = 1; L < n; L *= 2) {
-    fill(cnt.begin(), cnt.end(), 0);
-    for (int i = 0; i < n; i++) {
-      cnt[c[i]]++;
+    vector<int> cnt(n);
+    for (auto i: c) {
+      cnt[i]++;
     }
-    pos[0] = 0;
-    for (int i = 1; i < n; i++) {
-      pos[i] = pos[i - 1] + cnt[i - 1];
-    }
-    for (int i = 0; i < n; i++) {
-      suf[i] = (suf[i] - L + n) % n;
-    }
-    vector<int> new_suf(n), new_c(n);
-    for (int i = 0; i < n; i++) {
-      int where = pos[c[suf[i]]];
-      new_suf[where] = suf[i];
-      pos[c[suf[i]]]++;
-    }
-    cls = -1;
-    for (int i = 0; i < n; i++) {
-      if (i == 0) {
-        cls++;
-        new_c[new_suf[i]] = cls;
-        continue;
-      }
-      pair<int, int> prev = {c[new_suf[i - 1]], c[(new_suf[i - 1] + L) % n]};
-      pair<int, int> now = {c[new_suf[i]], c[(new_suf[i] + L) % n]};
-      if (prev != now) {
-        cls++;
-      }
-      new_c[new_suf[i]] = cls;
-    }
-    swap(c, new_c);
-    swap(suf, new_suf);
-    if (cls == n - 1) {
+    if (*min_element(all(cnt)) > 0) {
       break;
     }
+    vector<int> pref(n);
+    for (int i = 1; i < n; i++) {
+      pref[i] = pref[i - 1] + cnt[i - 1];
+    }
+    vector<int> na(n);
+    for (int i = 0; i < n; i++) {
+      int pos = (a[i] - L + n) % n;
+      na[pref[c[pos]]++] = pos;
+    }
+    a = na;
+    vector<int> nc(n);
+    cc = 0;
+    for (int i = 0; i < n; i++) {
+      if (i == 0 || c[a[i]] != c[a[i - 1]] ||
+          c[(a[i] + L) % n] != c[(a[i - 1] + L) % n]) {
+        nc[a[i]] = cc++;
+      } else {
+        nc[a[i]] = nc[a[i - 1]];
+      }
+    }
+    c = nc;
   }
-  suf.erase(suf.begin());
-  return suf;
+  // Remove, if you want to sort cyclic shifts
+  a.erase(a.begin());
+  s.pop_back();
+  return a;
 }
 
 vector<int> kasai(string s, vector<int> sa) {
