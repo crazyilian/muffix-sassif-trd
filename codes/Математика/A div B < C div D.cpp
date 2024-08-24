4032181e96550c98c3e654e70b982ce8d0435e78
Ilian Andrianov <ilian.and.04@gmail.com>
2024-08-24 15:40:37 +0300

    A/B < C/D

---
+++ codes/Математика/A div B < C div D.cpp
@@ -0,0 +1,17 @@
+char sign(int x) {
+  return x < 0 ? -1 : x > 0;
+}
+
+// -1 = less, 0 = equal, 1 = greater
+char is_less(int a, int b, int c, int d) {
+  if (a / b != c / d)
+    return sign(a / b - c / d);
+  a = a % b;
+  c = c % d;
+  if (a == 0)
+    return -sign(c) * sign(d);
+  if (c == 0)
+    return sign(a) * sign(b);
+  return is_less(d, c, b, a) * sign(a) * sign(b) * sign(c) * sign(d);
+}
+
