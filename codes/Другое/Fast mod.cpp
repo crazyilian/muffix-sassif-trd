7ef09a285811387d8d522e3428484a351390246f
Ilian Andrianov <ilian.and.04@gmail.com>
2024-08-24 22:05:49 +0300

    fast mod fix

--- codes/Другое/Fast mod.cpp
+++ codes/Другое/Fast mod.cpp
@@ -1,14 +1,13 @@
 // Быстрое взятие по НЕ константному модулю (~1.5 раза быстрее)
 struct FastMod {
-  ull mod;
-  __uint128_t wtf;
+  ull mod, wtf;
   FastMod(ull mod) : mod(mod), wtf((__uint128_t(1) << 64) / mod) {}
   ull reduce(ull a) const {
-    ull q = (wtf * a) >> 64;
+    ull q = ((__uint128_t)wtf * a) >> 64;
     ull r = a - q * mod;
     return r >= mod ? r - mod : r;
   }
-};
+}; // Usage:
 // FastMod F(m);
 // ull x_mod_m = F.reduce(x);
 
