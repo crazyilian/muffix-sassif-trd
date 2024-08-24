70958016cfac0399233b27b6bd699b74ab5af7bc
tem-shett <115083378+tem-shett@users.noreply.github.com>
2024-08-24 16:33:01 +0300

    кхт на макс/мин, равные k

--- codes/ДП/CHT.cpp
+++ codes/ДП/CHT.cpp
@@ -1,39 +1,42 @@
-struct Line {
-  ld k, b;
+struct line {
+    int k, b;
+    int eval(int x) {
+      return k * x + b;
+    }
 };
-
-pair<ld, ld> inter(Line a, Line b) {
-  ld x = (b.b - a.b) / (a.k - b.k);
-  ld y = a.k * x + a.b;
-  return {x, y};
+struct part {
+    line a;
+    double x;
+};
+double intersection(line a, line b) {
+  return (a.b - b.b) / (double) (b.k - a.k);
 }
+struct ConvexHull {
+    // for min: k decreasing (non-increasing)
+    // for max: k increasing (non-decreasing)
+    vector<part> st;
 
-void add_line(ld k, ld b, vector<Line> &s, vector<pair<ld, ld>> &pts) {
-  while (s.size() >= 2) {
-    pair<ld, ld> x1 = inter(s.back(), s[s.size() - 2]);
-    pair<ld, ld> x2 = inter(s[s.size() - 2], {k, b});
-    if (x1 > x2) {
-      break;
+    void add(line a) {
+      if (!st.empty() && st.back().a.k == a.k) {
+        if (st.back().a.b < a.b) st.pop_back(); // for max
+//        if (st.back().a.b > a.b) st.pop_back(); // for min
+        else return;
+      }
+      while (st.size() > 1 &&
+             intersection(st[st.size() - 2].a, a) <= st[st.size() - 2].x)
+        st.pop_back();
+      if (!st.empty()) st.back().x = intersection(st.back().a, a);
+      st.push_back({a, INF});
+      // INF = max intersection point
     }
-    pts.pop_back();
-    s.pop_back();
-  }
-  if (!s.empty()) {
-    pts.push_back(inter(s.back(), {k, b}));
-  }
-  s.push_back({k, b});
-}
 
-ld bin_search(vector<Line> &s, ld x) {
-  int l = 0, r = s.size();
-  while (l + 1 < r) {
-    int m = (r + l) / 2;
-    auto kek = inter(s[m - 1], s[m]);
-    if (kek.first >= x) {
-      l = m;
-    } else {
-      r = m;
+    int get_val(int x) {
+      int l = -1, r = (int) st.size() - 1;
+      while (r - l > 1) {
+        int m = (l + r) / 2;
+        if (st[m].x < x) l = m;
+        else r = m;
+      }
+      return st[r].a.eval(x);
     }
-  }
-  return s[l].k * x + s[l].b;
-}
+};
