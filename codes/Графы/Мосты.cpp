f05d8212f549bd14a88f919b243e81eeedff4213
Ilian Andrianov <ilian.and.04@gmail.com>
2024-11-16 15:51:25 +0300

    Update Мосты.cpp

--- codes/Графы/Мосты.cpp
+++ codes/Графы/Мосты.cpp
@@ -1,16 +1,16 @@
-void dfs(int v, int par) {
+// graph[v][i] = {u, edge_i}
+void dfs(int v, int pi = -1) {
   vis[v] = 1;
   up[v] = tin[v] = timer++;
-  for (auto u : g[v]) {
+  for (auto [u, ei] : g[v]) {
     if (!vis[u]) {
       dfs(u, v);
       up[v] = min(up[v], up[u]);
-    } else if (u != par) {
+    } else if (ei != pi)
       up[v] = min(up[v], tin[u]);
-    }
     if (up[u] > tin[v]) {
       bridges.emplace_back(v, u);
+      is_bridge[ei] = 1;
     }
   }
 }
-
