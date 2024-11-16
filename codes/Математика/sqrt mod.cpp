218d7d6699a9f9af6a874f3adbf86129f5a661fc
Ilian Andrianov <ilian.and.04@gmail.com>
2024-11-16 16:28:11 +0300

    sqrt mod

---
+++ codes/Математика/sqrt mod.cpp
@@ -0,0 +1,27 @@
+// -1 if no solution
+// x=sqrt(a, p) => x^2=a and (-x)^2=a;
+// should be changed if const p
+ll sqrt(ll a, ll p) {
+  a %= p;
+  if (a < 0) a += p;
+  if (a == 0) return 0;
+  if (binpow(a, (p - 1) / 2, p) != 1)
+    return -1; // no solution
+  if (p % 4 == 3) return binpow(a, (p + 1) / 4, p);
+  ll s = p - 1, n = 2;
+  int r = 0, m;
+  while (s % 2 == 0) ++r, s /= 2;
+  while (binpow(n, (p - 1) / 2, p) != p - 1) ++n;
+  ll x = binpow(a, (s + 1) / 2, p);
+  ll b = binpow(a, s, p), g = binpow(n, s, p);
+  for (;; r = m) {
+    ll t = b;
+    for (m = 0; m < r && t != 1; ++m) t = t * t % p;
+    if (m == 0) return x;
+    ll gs = binpow(g, 1LL << (r - m - 1), p);
+    g = gs * gs % p;
+    x = x * gs % p;
+    b = b * g % p;
+  }
+}
+
