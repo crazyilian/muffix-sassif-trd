cfa07d27c77b2d7d40be9d77e542e24af21f8c37
Ilian Andrianov <ilian.and.04@gmail.com>
2025-01-15 15:51:23 +0300

    Update Диофантовы уравнения.cpp
    меньше код, чуть быстрее работает, исправлены переполнения

--- codes/Математика/Диофантовы уравнения.cpp
+++ codes/Математика/Диофантовы уравнения.cpp
@@ -2,38 +2,27 @@ pair<int, int> ext_gcd(int a, int b) {
   int x1 = 1, y1 = 0, x2 = 0, y2 = 1;
   while (b) {
     int k = a / b;
-    x1 = x1 - x2 * k;
-    y1 = y1 - y2 * k;
-    swap(x1, x2);
-    swap(y1, y2);
+    x1 -= x2 * k;
+    y1 -= y2 * k;
     a %= b;
-    swap(a, b);
+    swap(x1, x2), swap(y1, y2), swap(a, b);
   }
   return {x1, y1};
 }
 
+// solve $ax+by=c$ with minimum $x \geq 0$
 bool cool_ext_gcd(int a, int b, int c, int &x, int &y) {
-  if (b == 0) {
-    y = 0;
-    if (a == 0) {
-      x = 0;
-      return c == 0;
-    } else {
-      x = c / a;
-      return c % a == 0;
+    if (b == 0) {
+        y = 0;
+        if (a == 0)
+            return x = 0, c == 0;
+        return x = c / a, c % a == 0;
     }
-  }
-  auto [x0, y0] = ext_gcd(a, b);
-  int g = x0 * a + y0 * b;
-  if (c % g != 0)
-    return false;
-  x0 *= c / g;
-  y0 *= c / g;
-  int t = b / g;
-  int k = (-x0) / t;
-  if (x0 + t * k < 0)
-    k += t / abs(t);
-  x = x0 + t * k;
-  y = y0 - (a / g) * k;
-  return true;
+    auto [x0, y0] = ext_gcd(a, b);
+    int g = (ll)x0 * a + (ll)y0 * b;
+    if (c % g != 0) return false;
+    x = (ll)x0 * (c / g) % (b / g);
+    if (x < 0) x += abs(b / g);
+    y = (c - (ll)a * x) / b;
+    return true;
 }
