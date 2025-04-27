bf8d510c8e90fcafceb7b45567d810b3358c9031
Ilian Andrianov <ilian.and.04@gmail.com>
2025-04-27 21:09:48 +0300

    refactor Fast mod.cpp

--- codes/Другое/Fast mod.cpp
+++ codes/Другое/Fast mod.cpp
@@ -1,12 +1,12 @@
-// Быстрое взятие по НЕ константному модулю (в 1.5-1.9 раза быстрее)
+// Быстрое взятие по НЕ константному модулю (в 2-4 раза быстрее)
 struct FastMod {
   ull b, m;
   FastMod(ull b) : b(b), m(-1ULL / b) {}
-  ull reduce(ull a) const {
+  ull mod(ull a) const {
       ull r = a - (ull)((__uint128_t(m) * a) >> 64) * b;
-      return r;  // r in [0, 2b) // x1.9 speed
-      return r >= b ? r - b : r; // x1.5 speed
+      return r;  // r in [0, 2b) // $\approx$ x3.5 speed
+      return r >= b ? r - b : r; // $\approx$ x3 speed
   }
 }; // Usage:
 // FastMod F(m);
-// ull x_mod_m = F.reduce(x);
+// ull x_mod_m = F.mod(x);
