f22276f402378d54dbe42ca2f7d0f03879a76660
Ilian Andrianov <ilian.and.04@gmail.com>
2025-07-09 17:53:09 +0300

    Update Лежит ли точка в многоугольнике.cpp

--- codes/Геометрия/Лежит ли точка в многоугольнике.cpp
+++ codes/Геометрия/Лежит ли точка в многоугольнике.cpp
@@ -1,13 +1,26 @@
 // Выпуклый многоугольник, P[0] = minvctr
 bool is_point_in_poly(vctr A, vector<vctr> &P) {
-  int n = P.size();
-  int ind = lower_bound(P.begin(), P.end(), A, cmp_convex_hull) - P.begin();
-  if (ind == n || ind == 0)
+  auto tmp = A - P[0];
+  if (sign(tmp.y) < 0 || (sign(tmp.y) == 0 && sign(tmp.x) < 0))
     return false;
-  if (ind == 0)
-    ind++;
+  if (sign(tmp.y) == 0 && sign(tmp.x) == 0)
+    return true;
+  int ind = lower_bound(P.begin(), P.end(), A, cmp_convex_hull) - P.begin();
+  assert(ind != 0);
+  if (ind == P.size())
+    return false;
   vctr B = A - P[ind - 1];
   vctr C = P[ind] - P[ind - 1];
   return sign(C * B) >= 0;
+}
+
+bool is_point_in_poly_strict(vctr A, vector<vctr> &P) {
+  if (sign(A.y - P[0].y) <= 0 || sign((A - P[0]) * (P.back() - P[0])) <= 0)
+      return false;
+  int ind = lower_bound(P.begin(), P.end(), A, cmp_convex_hull) - P.begin();
+  assert(ind != 0 && ind != P.size());
+  vctr B = A - P[ind - 1];
+  vctr C = P[ind] - P[ind - 1];
+  return sign(C * B) > 0;
 }
 
