ffd5002e61f3806976c32b04867eb97fda22f07d
Ilian Andrianov <ilian.and.04@gmail.com>
2025-09-02 01:39:50 +0300

    NN NRU HSE

--- gen.sh
+++ gen.sh
@@ -3,7 +3,7 @@
 notebook-generator/notebook-generator ./codes \
     --title "\\bf{Muffix Sassif -- TRD}" \
     --author "Andrianov, Lepeshov, Shulyatev" \
-    --initials "NRU HSE (Andrianov, Lepeshov, Shulyatev)" \
+    --initials "Nizhny Novgorod Branch of HSE University (Andrianov, Lepeshov, Shulyatev)" \
     --output ./teambook.pdf \
     --image ./notebook-generator/picture.jpg \
     --assets `pwd`/assets \
