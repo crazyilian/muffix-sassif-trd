55e3b605caa8ccd341c1afe249ec7e821f796e8a
sevlll <82900814+sevlll@users.noreply.github.com>
2025-09-02 14:10:24 +0300

    Lowerbound Flow кодиком

---
+++ codes/Графы/Lowerbound Flow.cpp
@@ -0,0 +1,45 @@
+struct LowerBoundMinFlow {
+    Dinic dinic;
+    int S, T; // исток и сток
+    int Sx, Tx; // вспомогательные вершины, любые неиспользуемые индексы
+
+    LowerBoundMinFlow(int S_, int T_, int Sx_, int Tx_) : S(S_), T(T_), Sx(Sx_), Tx(Tx_) {}
+
+    void addedge(int v, int u, int mincap, int maxcap) {
+        // все рёбра ориентированные
+        dinic.addedge(v, u, maxcap - mincap);
+        dinic.addedge(Sx, u, mincap);
+        dinic.addedge(v, Tx, mincap);
+    }
+
+    bool check() {
+        for (auto edge: dinic.graph[Sx]) {
+            if (edge.f != edge.c) {
+                return false;
+            }
+        }
+        for (auto edge: dinic.graph[Tx]) {
+            auto iedge = dinic.graph[edge.u][edge.r];
+            if (iedge.f != iedge.c) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool only_existence() {
+        dinic.addedge(T, S, INF);
+        dinic.run(Sx, Tx);
+        return check();
+    }
+
+    bool with_flows() {
+        dinic.run(Sx, Tx);
+        dinic.run(Sx, T);
+        dinic.run(S, Tx);
+        dinic.run(S, T);
+        // real (v, u, mincap, maxcap) flow = flow on (v, u, maxcap - mincap) edge + mincap
+        return check();
+    }
+
+};
