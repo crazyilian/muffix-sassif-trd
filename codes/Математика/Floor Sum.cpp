950658d5aaaa2eb988e4cae9ab52989039502401
sevlll <82900814+sevlll@users.noreply.github.com>
2025-10-25 23:28:37 +0300

    2-отступ Floor Sum.cpp

--- codes/Математика/Floor Sum.cpp
+++ codes/Математика/Floor Sum.cpp
@@ -1,12 +1,12 @@
 int floor_sum(int n, int d, int m, int a) {
-    // sum_{i=0}^{n-1} floor((a + i*m)/d), only non-negative integers!
-    int ans = 0;
-    ans += (n * (n - 1) / 2) * (m / d);
-    m %= d;
-    ans += n * (a / d);
-    a %= d;
-    int l = m * n + a;
-    if (l >= d)
-        ans += floor_sum(l / d, m, d, l % d);
-    return ans;
+  // sum_{i=0}^{n-1} floor((a + i*m)/d), only non-negative integers!
+  int ans = 0;
+  ans += (n * (n - 1) / 2) * (m / d);
+  m %= d;
+  ans += n * (a / d);
+  a %= d;
+  int l = m * n + a;
+  if (l >= d)
+    ans += floor_sum(l / d, m, d, l % d);
+  return ans;
 }
