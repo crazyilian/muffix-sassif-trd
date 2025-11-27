c93fd499e39eaec1ece608f42242088c927456fa
Ilian Andrianov <ilian.and.04@gmail.com>
2025-11-28 01:49:04 +0300

    Update Точки сочленения.cpp

--- codes/Графы/Точки сочленения.cpp
+++ codes/Графы/Точки сочленения.cpp
@@ -1,21 +1,18 @@
-void dfs(int v, int par) {
+void dfs(int v, int p = -1) {
   vis[v] = 1;
   up[v] = tin[v] = timer++;
+  bool is_artic = false;
   int child = 0;
   for (auto u : g[v]) {
     if (!vis[u]) {
+      child++;
       dfs(u, v);
       up[v] = min(up[v], up[u]);
-      if (up[u] >= tin[v] && par != -1) {
-        points.insert(v);
-      }
-      child++;
-    } else if (u != par) {
+      is_artic |= p != -1 && up[u] >= tin[v];
+    } else
       up[v] = min(up[v], tin[u]);
-    }
   }
-  if (par == -1 && child >= 2) {
-    points.insert(v);
-  }
+  is_artic |= p == -1 && child >= 2;
+  if (is_artic)
+    articulation_points.insert(v);
 }
-
