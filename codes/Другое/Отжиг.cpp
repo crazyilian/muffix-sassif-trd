a36f6044e47f28355b404f009e2b38a504f0e3bc
Ilian Andrianov <ilian.and.04@gmail.com>
2026-01-07 19:15:59 +0300

    fix typo

--- codes/Другое/Отжиг.cpp
+++ codes/Другое/Отжиг.cpp
@@ -1,15 +1,15 @@
 const double lambda = 0.999;
-double temprature = 1;
+double temperature = 1;
 mt19937 rnd(777);
 
 double gen_rand_01() {
   return rnd() / (double) UINT32_MAX;
 }
 bool f(int delta) {
-  return exp(-delta / temprature) > gen_rand_01();
+  return exp(-delta / temperature) > gen_rand_01();
 }
 void make_change() {
-  temprature *= lambda;
+  temperature *= lambda;
   // calc change score
   if (change_score <= 0 || f(change_score)) {
     score += change_score;
