9285a382648b1e505e146e9fe6a41f8f624c173b
Ilian Andrianov <ilian.and.04@gmail.com>
2026-03-30 21:15:46 +0300

    Much faster LiChao

--- codes/ДП/Li Chao.cpp
+++ codes/ДП/Li Chao.cpp
@@ -1,55 +1,47 @@
-// MAXIMUM
 struct Line {
-  int k, b;
-
-  int f(int x) {
-    return k * x + b;
-  }
+  ll k, b;
+  ll operator()(ll x) const { return k * x + b; }
 };
 
-struct ST {
-  vector<Line> st;
+// LiChao for maximum at x
+struct LiChao {
+  struct Node {
+    int l, r; 
+    Line ln;
+  };
+  ll L, R;
+  int root;
+  vector<Node> t;
 
-  ST(int n) {
-    Line ln = {0LL, -INF};
-    st.resize(4 * n, ln);
+  LiChao(ll L, ll R, int maxsz = 0) : L(L), R(R), root(0) {
+    t.reserve(maxsz + 1);
+    t.pb({0, 0, {0, -INF}}); // t[0] = empty. +INF for min
   }
 
-  void upd(int i, int l, int r, Line ln) {
-    int child = 1;
-    Line ln1 = ln;
-    int m = (l + r) / 2;
-    if (ln.f(m) > st[i].f(m)) {
-      if (ln.k < st[i].k) {
-        child = 2;
-      }
-      ln1 = st[i];
-      st[i] = ln;
-    } else {
-      if (st[i].k < ln.k) {
-        child = 2;
-      }
+  void add(int &v, ll l, ll r, Line ln) {
+    if (!v) {
+      v = t.size();
+      t.push_back({0, 0, ln});
+      return;
     }
-    if (l + 1 < r) {
-      if (child == 1) {
-        upd(i * 2 + 1, l, m, ln1);
-      } else {
-        upd(i * 2 + 2, m, r, ln1);
-      }
-    }
+    ll m = (l + r) / 2;
+    if (ln(m) > t[v].ln(m)) swap(ln, t[v].ln); // "<" for min
+    if (r - l == 1 || ln.k == t[v].ln.k) return;
+    if (ln.k < t[v].ln.k) add(t[v].l, l, m, ln); // "<" for min
+    else add(t[v].r, m, r, ln);
   }
+  void add(Line nw) { add(root, L, R, nw); }
 
-  int res(int i, int l, int r, int x) {
-    if (l + 1 == r) {
-      return st[i].f(x);
+  ll get(ll x) {
+    ll ans = -INF; // +INF for min
+    int v = root;
+    ll l = L, r = R;
+    while (v) {
+      ans = max(ans, t[v].ln(x)); // "min" for min
+      ll m = (l + r) / 2;
+      if (x < m) v = t[v].l, r = m;
+      else v = t[v].r, l = m;
     }
-    int m = (l + r) / 2;
-    int val = st[i].f(x);
-    if (x < m) {
-      val = max(val, res(i * 2 + 1, l, m, x));
-    } else {
-      val = max(val, res(i * 2 + 2, m, r, x));
-    }
-    return val;
+    return ans;
   }
 };
