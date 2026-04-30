73964d6466a67bc1efa372ee2e3569c77b05632b
tem-shett <115083378+tem-shett@users.noreply.github.com>
2026-05-01 02:47:02 +0300

    Update Li Chao.cpp

--- codes/ДП/Li Chao.cpp
+++ codes/ДП/Li Chao.cpp
@@ -6,31 +6,45 @@ struct Line {
 // LiChao for maximum at x
 struct LiChao {
   struct Node {
-    int l, r; 
+    int l, r;
     Line ln;
   };
   ll L, R;
   int root;
   vector<Node> t;
 
-  LiChao(ll L, ll R, int maxsz = 0) : L(L), R(R), root(0) {
+  LiChao(ll L, ll R, int maxsz=1) : L(L), R(R), root(0) {
     t.reserve(maxsz + 1);
     t.pb({0, 0, {0, -INF}}); // t[0] = empty. +INF for min
   }
+  
+  int add_on_seg(int v, ll l, ll r, ll ql, ll qr, Line ln) {
+    if (qr <= l || r <= ql) { return v; }
+    if (ql <= l && r <= qr) { return add(v, l, r, ln); }
+    if (!v) { v = t.size(), t.push_back({0, 0, {0, -INF}}); }
+    ll m = (l + r) / 2;
+    t[v].l = add_on_seg(t[v].l, l, m, ql, qr, ln);
+    t[v].r = add_on_seg(t[v].r, m, r, ql, qr, ln);
+    return v;
+  }
+  void add_on_seg(ll ql, ll qr, Line ln) { // [ql,qr)
+    root = add_on_seg(root, L, R, ql, qr, ln);
+  }
 
-  void add(int &v, ll l, ll r, Line ln) {
+  int add(int v, ll l, ll r, Line ln) {
     if (!v) {
       v = t.size();
       t.push_back({0, 0, ln});
-      return;
+      return v;
     }
     ll m = (l + r) / 2;
     if (ln(m) > t[v].ln(m)) swap(ln, t[v].ln); // "<" for min
-    if (r - l == 1 || ln.k == t[v].ln.k) return;
-    if (ln.k < t[v].ln.k) add(t[v].l, l, m, ln); // ">" for min
-    else add(t[v].r, m, r, ln);
+    if (r - l == 1 || ln.k == t[v].ln.k) return v;
+    if (ln.k < t[v].ln.k) t[v].l = add(t[v].l, l, m, ln); // ">" for min
+    else t[v].r = add(t[v].r, m, r, ln);
+    return v;
   }
-  void add(Line nw) { add(root, L, R, nw); }
+  void add(Line nw) { root = add(root, L, R, nw); }
 
   ll get(ll x) {
     ll ans = -INF; // +INF for min
