c2d6740220c74107b2ed141268ea250a5b700a87
Ilian Andrianov <ilian.and.04@gmail.com>
2026-08-24 01:16:23 +0300

    Refactor compare function for improved readability

--- codes/Математика/AdivB cmp CdivD.cpp
+++ codes/Математика/AdivB cmp CdivD.cpp
@@ -1,17 +1,8 @@
-char sign(ll x) {
-  return x < 0 ? -1 : x > 0;
-}
-
 // -1 = less, 0 = equal, 1 = greater
 char compare(ll a, ll b, ll c, ll d) {
-  if (a / b != c / d)
-    return sign(a / b - c / d);
-  a = a % b;
-  c = c % d;
-  if (a == 0)
-    return -sign(c) * sign(d);
-  if (c == 0)
-    return sign(a) * sign(b);
+  if (a / b != c / d) return sign(a / b - c / d);
+  a %= b, c %= d;
+  if (a == 0) return -sign(c) * sign(d);
+  if (c == 0) return sign(a) * sign(b);
   return compare(d, c, b, a) * sign(a) * sign(b) * sign(c) * sign(d);
 }
-
