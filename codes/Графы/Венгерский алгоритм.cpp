c7d0212869e940e368e77af6cc301ab362292936
Ilian Andrianov <ilian.and.04@gmail.com>
2026-09-01 19:43:05 +0300

    venger easier naming

--- codes/Графы/Венгерский алгоритм.cpp
+++ codes/Графы/Венгерский алгоритм.cpp
@@ -4,37 +4,39 @@ pair<int, vector<int>> venger(vector<vector<int>> a) {
 // a - массив весов $(n+1) \times (m+1)$
 // a[0][..] = a[..][0] = 0
 // возвращает ans[i] = j если взяли ребро a[i][j]
-  int n = (int) a.size() - 1;
-  int m = (int) a[0].size() - 1;
+  int n = (int)a.size() - 1;
+  int m = (int)a[0].size() - 1;
   vector<int> u(n + 1), v(m + 1), p(m + 1), way(m + 1);
   for (int i = 1; i <= n; ++i) {
     p[0] = i;
-    int j0 = 0;
+    int y = 0;
     vector<int> minv(m + 1, INF);
     vector<char> used(m + 1, false);
     do {
-      used[j0] = true;
-      int i0 = p[j0], delta = INF, j1;
-      for (int j = 1; j <= m; ++j)
+      used[y] = true;
+      int x = p[y], delta = INF, z;
+      for (int j = 1; j <= m; ++j) {
         if (!used[j]) {
-          int cur = a[i0][j] - u[i0] - v[j];
+          int cur = a[x][j] - u[x] - v[j];
           if (cur < minv[j])
-            minv[j] = cur, way[j] = j0;
+            minv[j] = cur, way[j] = y;
           if (minv[j] < delta)
-            delta = minv[j], j1 = j;
+            delta = minv[j], z = j;
         }
-      for (int j = 0; j <= m; ++j)
+      }
+      for (int j = 0; j <= m; ++j) {
         if (used[j])
           u[p[j]] += delta, v[j] -= delta;
         else
           minv[j] -= delta;
-      j0 = j1;
-    } while (p[j0] != 0);
+      }
+      y = z;
+    } while (p[y] != 0);
     do {
-      int j1 = way[j0];
-      p[j0] = p[j1];
-      j0 = j1;
-    } while (j0);
+      int j1 = way[y];
+      p[y] = p[j1];
+      y = j1;
+    } while (y);
   }
   int cost = -v[0];
   vector<int> ans(n + 1);
