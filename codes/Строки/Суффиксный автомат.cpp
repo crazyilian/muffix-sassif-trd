9b166a6b92021dd798bd028ef42ed581b0f4c5a5
Ilian Andrianov <ilian.and.04@gmail.com>
2026-09-02 03:19:34 +0300

    add firstpos to sam

--- codes/Строки/Суффиксный автомат.cpp
+++ codes/Строки/Суффиксный автомат.cpp
@@ -6,12 +6,12 @@ int ans = 0;
 struct Node {
   int go[SIGMA];
   int s, p;
-  int len;
+  int len, firstpos;
 
   Node() {
     fill(go, go + SIGMA, -1);
     s = -1, p = -1;
-    len = 0;
+    len = 0, firstpos = -1;
   }
 };
 
@@ -21,6 +21,7 @@ int add(int A, int ch, vector<Node> &sa) {
   sa[B].p = A;
   sa[B].s = 0;
   sa[B].len = sa[A].len + 1;
+  sa[B].firstpos = sa[A].firstpos + 1;
   for (; A != -1; A = sa[A].s) {
     if (sa[A].go[ch] == -1) {
       sa[A].go[ch] = B;
@@ -36,8 +37,8 @@ int add(int A, int ch, vector<Node> &sa) {
     sa[D].s = sa[C].s;
     sa[D].p = A;
     sa[D].len = sa[A].len + 1;
-    sa[C].s = D;
-    sa[B].s = D;
+    sa[D].firstpos = sa[C].firstpos;
+    sa[C].s = sa[B].s = D;
     copy(sa[C].go, sa[C].go + SIGMA, sa[D].go);
     for (; A != -1 && sa[A].go[ch] == C; A = sa[A].s)
       sa[A].go[ch] = D;
